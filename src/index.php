a22fb5748d23c83c334f526d88c3f06c71be7e03
albertine <albiradu@gmail.com>
2025-02-06 12:15:45 +0200

    index page is created

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container text-center">
-    <h1 class="mt-5">Welcome To New Staff MIS</h1>
+    <h1 class="mt-5">Welcome To to students MIS</h1>
     <p><a href="registration.php" class="btn btn-primary mt-3">Go to Registration Page</a></p>
     <p><a href="login.php" class="btn btn-secondary mt-3">Go to Login Page</a></p>
 </body>
