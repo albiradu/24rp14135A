2e722a5bd566b10734a7ff011524a8f617f47997
albertine <albiradu@gmail.com>
2025-02-06 11:42:50 +0200

    index page is created

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - NEW STAFF</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="container text-center">
+    <h1 class="mt-5">Welcome To New Staff MIS</h1>
+    <p><a href="registration.php" class="btn btn-primary mt-3">Go to Registration Page</a></p>
+    <p><a href="login.php" class="btn btn-secondary mt-3">Go to Login Page</a></p>
+</body>
+</html>
